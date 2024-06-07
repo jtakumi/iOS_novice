5c7957fe5253735491c3aeb30ea7346feaae5a6d
jtakumi <takumi87jn@gmail.com>
2024-06-07 23:27:41 +0900

    circle image

---
+++ iOS_novice/iOS_novice/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  iOS_novice
+//
+//  Created by Takumi Jindo on 2024/06/07.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("theTree")
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white,lineWidth: 4)
+            }
+            .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleImage()
+}
