40d10a934b792c855b30081662e060cd511334ec
jtakumi <takumi87jn@gmail.com>
2024-06-07 22:53:19 +0900

    initial commit

---
+++ iOS_novice/iOS_novice/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  iOS_novice
+//
+//  Created by Takumi Jindo on 2024/06/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, iOS development!")
+            Text("I'm j____takumi.")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOS_novice/iOS_novice/iOS_noviceApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_noviceApp.swift
+//  iOS_novice
+//
+//  Created by Takumi Jindo on 2024/06/07.
+//
+
+import SwiftUI
+
+@main
+struct iOS_noviceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
