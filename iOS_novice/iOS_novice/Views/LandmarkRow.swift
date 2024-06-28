5d9f3085297b1ba7796dfd3674086362e76ac01f
jtakumi <takumi87jn@gmail.com>
2024-06-29 08:45:12 +0900

    LandmarkRow add

---
+++ iOS_novice/iOS_novice/Views/LandmarkRow.swift
@@ -0,0 +1,26 @@
+//
+//  LandmarkRow.swift
+//  iOS_novice
+//
+//  Created on 2024/06/29.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark:Landmark
+    
+    var body: some View {
+        HStack{
+            landmark.image
+                .resizable()
+                .frame(width: 50,height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark:landmarks[0])
+}
