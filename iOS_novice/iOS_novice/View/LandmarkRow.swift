dc2b843eb2e747463954c1a47aef13c214b9add6
jtakumi <takumi87jn@gmail.com>
2024-06-29 12:44:25 +0900

    add multiple preview in landmarkRow

--- iOS_novice/iOS_novice/View/LandmarkRow.swift
+++ iOS_novice/iOS_novice/View/LandmarkRow.swift
@@ -20,6 +20,18 @@ struct LandmarkRow: View {
     }
 }
 
-#Preview {
+#Preview("Turtle Rock") {
     LandmarkRow(landmark: landmarks[0])
 }
+
+#Preview("Salmon") {
+    LandmarkRow(landmark: landmarks[1])
+}
+
+#Preview("Group") {
+    Group{
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])
+
+    }
+   }
