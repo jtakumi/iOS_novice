c92645b867261efb09deca79a656e59b87a63d3b
jtakumi <takumi87jn@gmail.com>
2024-06-29 09:06:45 +0900

    Landmark.swift

---
+++ iOS_novice/iOS_novice/Landmark.swift
@@ -0,0 +1,31 @@
+//
+//  Landmark.swift
+//  iOS_novice
+//
+//  Created on 2024/06/29.
+//
+
+import Foundation
+import SwiftUI
+import CoreLocation
+
+struct Landmark:Hashable,Codable{
+    var id:Int
+    var name:String
+    var park:String
+    var state:String
+    var description:String
+    
+    private var coordinates:Coordinates
+    var locationCoordinate:CLLocationCoordinate2D{
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude
+        )
+    }
+    
+    struct Coordinates: Hashable,Codable {
+        var latitude:Double
+        var longitude:Double
+    }
+}
