79cbb88530a02c027d2ace6b7fa4ecd88defa698
jtakumi <takumi87jn@gmail.com>
2024-09-01 14:20:47 +0900

    function created

--- iOS_novice/iOS_novice/Model/UserFavOutput.swift
+++ iOS_novice/iOS_novice/Model/UserFavOutput.swift
@@ -5,3 +5,18 @@
 //
 
 import Foundation
+
+// 星アイコンがタップされたら観光地ID と お気に入りフラグを JSON ファイルとして書き出す
+func UserFavOutput(landmarkId:Int,isFavorite:Bool){
+  if let outputData = "{\(landmarkId):\(isFavorite)}".data(using: .utf8),
+     let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first{
+      let outputFileName = documentDirectory.appendingPathComponent("UserFavOutput.json")
+        do{
+            try outputData.write(to: outputFileName)
+        } catch{
+            fatalError("Couldn't write \(outputData) to \(outputFileName) .")
+        }
+  } else {
+      print("outputData couldn't convert to json file.")
+  }
+}
