d5f3815a256c4a35a661baf36de055bd87058c46
jtakumi <takumi87jn@gmail.com>
2024-12-22 20:38:06 +0900

    modelContext.save()

--- iOS_novice/iOS_novice/Helpers/FavoriteButton.swift
+++ iOS_novice/iOS_novice/Helpers/FavoriteButton.swift
@@ -6,21 +6,30 @@
 import SwiftUI
 
 struct FavoriteButton: View {
-    @Binding var isSet:Bool
+    @Binding var landmark:Landmark
+    @Environment(\.modelContext) var modelContext
+    
+    private func saveFavorite(){
+        do{
+            try modelContext.save()
+            print("save favorite success")
+        }catch{
+            print("Error of saving favorite:\(error)")
+        }
+    }
     var body: some View {
         Button{
             //トグルスイッチをボタンとして使う
-            isSet.toggle()
+            landmark.isFavorite.toggle()
+            
+            saveFavorite()
+            
             } label:{
                 // true の場合は星のアイコンを表示する
-                Label("Toggle Favorite",systemImage: isSet ? "star.fill":"star")
+                Label("Toggle Favorite",systemImage: landmark.isFavorite ? "star.fill":"star")
                 //アイコンのみを表示する
                     .labelStyle(.iconOnly)
-                    .foregroundStyle(isSet ? .yellow : .gray)
+                    .foregroundStyle(landmark.isFavorite ? .yellow : .gray)
         }
     }
-}
-
-#Preview {
-    FavoriteButton(isSet: .constant(true))
 }
